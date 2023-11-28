c6c325df999ca67d3410ae7367b7f05ceaf18803
KhailoAndrey <hap512@gmail.com>
2023-11-29 00:31:40 +0200

    main finish

--- src/Main/main.jsx
+++ src/Main/main.jsx
@@ -5,7 +5,9 @@ import {
   MainAddress,
   MainContacts,
   MainImage,
+  MainInfo,
   MainMail,
+  MainNotation,
   MainText,
   MainTitle,
   Paragraph,
@@ -18,28 +20,31 @@ const Main = () => {
   return (
     <>
       <Wrapper>
-        <MainTitle>
-          <TextTitle>renewable energy for any task</TextTitle>
-        </MainTitle>
-        <Paragraph>
-          <MainText>
-            Development and implementation of renewable non-polluting energy
-            sources, generating power generation using energy wind, sun, water,
-            biomass
-          </MainText>
-          <BtnMore>
-            <TextMore>Learn more</TextMore>
-            <ArrowRight />
-          </BtnMore>
-        </Paragraph>
+        <MainInfo>
+          <MainTitle>
+            <TextTitle>renewable energy for any task</TextTitle>
+          </MainTitle>
+          <Paragraph>
+            <MainText>
+              Development and implementation of renewable non-polluting energy
+              sources, generating power generation using energy wind, sun,
+              water, biomass
+            </MainText>
+            <BtnMore>
+              <TextMore>Learn more</TextMore>
+              <ArrowRight />
+            </BtnMore>
+          </Paragraph>
+        </MainInfo>
         <Line />
         <MainContacts>
           <MainAddress>
             79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
           </MainAddress>
           <MainMail>office@ecosolution.com</MainMail>
+          <MainNotation>ecosolution © 2023</MainNotation>
         </MainContacts>
-        <MainImage/>
+        <MainImage />
       </Wrapper>
     </>
   );
--- src/Main/main.styled.js
+++ src/Main/main.styled.js
@@ -1,21 +1,33 @@
 import styled from 'styled-components';
 import { ReactSVG } from 'react-svg';
 import arrowRight from '../images/arrow-right.svg';
-import mainImg from '../images/wind-turbine.png'
+import mainImgMob from '../images/wind-turbine_mob.png'
+import mainImgTab from '../images/wind-turbine_tab.png'
+import mainImgDesk from '../images/wind-turbine_desk.png'
 
 export const Wrapper = styled.div`
   margin: 0;
   margin-top: 146px;
   @media screen and (min-width: 768px) {
     margin-top: 164px;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 24px;
+    /* display: flex; */
+    /* justify-content: space-between; */
+    /* margin-bottom: 24px; */
   }
   @media screen and (min-width: 1280px) {
     margin-top: 200px;
   }
 `;
+
+export const MainInfo = styled.div`
+@media screen and (min-width: 768px) {
+    display: flex;
+        justify-content: space-between;
+    margin-bottom: 24px;
+  }
+  
+`
+
 export const MainTitle = styled.div`
   width: 320px;
   margin-bottom: 24px;
@@ -59,7 +71,7 @@ export const Paragraph = styled.div`
 
   @media screen and (min-width: 768px) {
     max-width: 342px;
-    gap: 43px;
+    /* gap: 43px; */
     align-items: start;
     justify-content: space-between;
     margin-bottom: 0;
@@ -72,7 +84,7 @@ export const Paragraph = styled.div`
 `;
 export const MainText = styled.p`
   color: var(--main-clr-dark-green);
-  text-align: justify;
+  text-align: left;
   font-family: Fira Sans;
   font-size: 16px;
   font-style: normal;
@@ -146,6 +158,15 @@ export const MainContacts = styled.div`
   display: flex;
   gap: 8px;
   flex-direction: column;
+@media screen and (min-width: 768px) {
+margin-top: 16px;
+flex-direction: row;
+justify-content: space-between;
+gap: 0;
+}
+@media screen and (min-width: 1280px) {
+   margin-top: 12px;
+  }
 `
 export const MainAddress = styled.p`
   color: var(--main-clr-dark-green);
@@ -166,14 +187,53 @@ font-style: normal;
 font-weight: 400;
 line-height: normal;
 letter-spacing: -0.64px;
+@media screen and (min-width: 768px) {
+  margin-left: auto;
+}
 `
+
+export const MainNotation = styled.p`
+display: none;
+  color: var(--main-clr-dark-green);
+font-family: Fira Sans;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+letter-spacing: -0.64px;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    margin-left: 50px;
+    margin-right: 0;
+}
+@media screen and (min-width: 1280px) {
+  margin-left: 167px;
+}
+`
+
 export const MainImage = styled.img`
 width: 320px;
 height: 200px;
 flex-shrink: 0;
-background: url(${mainImg}) , 
+background: url(${mainImgMob}) , 
 lightgray 50%;
 background-position: center;
 background-size: cover;
-margin-top: 36px;
+margin: 36px 0;
+@media screen and (min-width: 768px) {
+margin: 40px 0 100px;
+background: url(${mainImgTab}), 
+lightgray 50%;
+height: 350px;
+width: 709px;
+}
+@media screen and (min-width: 1280px) {
+   background: url(${mainImgDesk}), 
+lightgray 50%;
+width: 1240px;
+height: 524px;
+flex-shrink: 0;
+margin: 36px 0 120px;
+
+  }
 `
