2fd4c4799aa0f444d9ba935ad189324692429a3d
KhailoAndrey <hap512@gmail.com>
2023-11-29 14:54:41 +0200

    Update main.styled.js

--- src/Main/main.styled.js
+++ src/Main/main.styled.js
@@ -1,18 +1,15 @@
 import styled from 'styled-components';
 import { ReactSVG } from 'react-svg';
 import arrowRight from '../images/arrow-right.svg';
-import mainImgMob from '../images/wind-turbine_mob.png'
-import mainImgTab from '../images/wind-turbine_tab.png'
-import mainImgDesk from '../images/wind-turbine_desk.png'
+import mainImgMob from '../images/wind-turbine_mob.png';
+import mainImgTab from '../images/wind-turbine_tab.png';
+import mainImgDesk from '../images/wind-turbine_desk.png';
 
 export const Wrapper = styled.div`
   margin: 0;
   margin-top: 146px;
   @media screen and (min-width: 768px) {
     margin-top: 164px;
-    /* display: flex; */
-    /* justify-content: space-between; */
-    /* margin-bottom: 24px; */
   }
   @media screen and (min-width: 1280px) {
     margin-top: 200px;
@@ -20,13 +17,12 @@ export const Wrapper = styled.div`
 `;
 
 export const MainInfo = styled.div`
-@media screen and (min-width: 768px) {
+  @media screen and (min-width: 768px) {
     display: flex;
-        justify-content: space-between;
+    justify-content: space-between;
     margin-bottom: 24px;
   }
-  
-`
+`;
 
 export const MainTitle = styled.div`
   width: 320px;
@@ -71,7 +67,6 @@ export const Paragraph = styled.div`
 
   @media screen and (min-width: 768px) {
     max-width: 342px;
-    /* gap: 43px; */
     align-items: start;
     justify-content: space-between;
     margin-bottom: 0;
@@ -158,82 +153,78 @@ export const MainContacts = styled.div`
   display: flex;
   gap: 8px;
   flex-direction: column;
-@media screen and (min-width: 768px) {
-margin-top: 16px;
-flex-direction: row;
-justify-content: space-between;
-gap: 0;
-}
-@media screen and (min-width: 1280px) {
-   margin-top: 12px;
+  @media screen and (min-width: 768px) {
+    margin-top: 16px;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 0;
   }
-`
+  @media screen and (min-width: 1280px) {
+    margin-top: 12px;
+  }
+`;
 export const MainAddress = styled.p`
   color: var(--main-clr-dark-green);
-font-family: Fira Sans;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-letter-spacing: -0.64px;  
-`
+  font-family: Fira Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.64px;
+`;
 export const MainMail = styled.a`
   text-decoration: none;
   cursor: pointer;
   color: var(--main-clr-dark-green);
-font-family: Fira Sans;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-letter-spacing: -0.64px;
-@media screen and (min-width: 768px) {
-  margin-left: auto;
-}
-`
+  font-family: Fira Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.64px;
+  @media screen and (min-width: 768px) {
+    margin-left: auto;
+  }
+`;
 
 export const MainNotation = styled.p`
-display: none;
+  display: none;
   color: var(--main-clr-dark-green);
-font-family: Fira Sans;
-font-size: 16px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-letter-spacing: -0.64px;
+  font-family: Fira Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.64px;
   @media screen and (min-width: 768px) {
     display: flex;
     margin-left: 50px;
     margin-right: 0;
-}
-@media screen and (min-width: 1280px) {
-  margin-left: 167px;
-}
-`
+  }
+  @media screen and (min-width: 1280px) {
+    margin-left: 167px;
+  }
+`;
 
 export const MainImage = styled.img`
-width: 320px;
-height: 200px;
-flex-shrink: 0;
-background: url(${mainImgMob}) , 
-lightgray 50%;
-background-position: center;
-background-size: cover;
-margin: 36px 0;
-@media screen and (min-width: 768px) {
-margin: 40px 0 100px;
-background: url(${mainImgTab}), 
-lightgray 50%;
-height: 350px;
-width: 709px;
-}
-@media screen and (min-width: 1280px) {
-   background: url(${mainImgDesk}), 
-lightgray 50%;
-width: 1240px;
-height: 524px;
-flex-shrink: 0;
-margin: 36px 0 120px;
-
+  width: 320px;
+  height: 200px;
+  flex-shrink: 0;
+  background: url(${mainImgMob}), lightgray 50%;
+  background-position: center;
+  background-size: cover;
+  margin: 36px 0;
+  @media screen and (min-width: 768px) {
+    margin: 40px 0 100px;
+    background: url(${mainImgTab}), lightgray 50%;
+    height: 350px;
+    width: 709px;
   }
-`
+  @media screen and (min-width: 1280px) {
+    background: url(${mainImgDesk}), lightgray 50%;
+    width: 1240px;
+    height: 524px;
+    flex-shrink: 0;
+    margin: 36px 0 120px;
+  }
+`;
