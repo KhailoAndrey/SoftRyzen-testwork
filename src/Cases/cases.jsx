25a9b338a231146310bea8e7e42324f97a4a6e9f
KhailoAndrey <hap512@gmail.com>
2023-12-01 11:34:01 +0200

    Update cases.jsx

--- src/Cases/cases.jsx
+++ src/Cases/cases.jsx
@@ -25,11 +25,11 @@ import {
   Wrapper,
 } from './cases.styled';
 
-import slide1 from '../images/slide1.jpg';
-import slide2 from '../images/slide2.jpg';
-import slide3 from '../images/slide3.jpg';
-import slide4 from '../images/slide4.jpg';
-import slide5 from '../images/slide5.jpg';
+// import slide1 from '../images/slide1.jpg';
+// import slide2 from '../images/slide2.jpg';
+// import slide3 from '../images/slide3.jpg';
+// import slide4 from '../images/slide4.jpg';
+// import slide5 from '../images/slide5.jpg';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
