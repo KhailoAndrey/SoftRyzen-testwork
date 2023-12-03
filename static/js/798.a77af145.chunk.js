6d9fd22740b183b4953bced411e7706812a44892
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2023-12-03 15:38:47 +0000

    Deploying to gh-pages from @ KhailoAndrey/SoftRyzen-testwork@da3e912fb6c94d3e9e501c93212e1c20d94318cb 🚀

---
+++ static/js/798.a77af145.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[798],{6798:(e,t,a)=>{e.exports=a.p+"static/media/arrow-right-up.23c258790bb727e343a5.svg"}}]);
---
+++ static/js/860.9f2aa840.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[860],{9860:(e,t,a)=>{e.exports=a.p+"static/media/close-light.dc24a051882f8b3588bf.svg"}}]);
